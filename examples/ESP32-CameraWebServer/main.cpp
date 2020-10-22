a5fe2f9af363ae99ea08ae872e8de0e7735594d9
Günter Schmitt <44770039+MasterSch@users.noreply.github.com>
2020-10-22 16:48:40 +0200

    Add files via upload

---
+++ examples/ESP32-CameraWebServer/main.cpp
@@ -0,0 +1,54 @@
+//
+//  von https://github.com/thorsten-l/ESP32-CameraWebServer
+//
+#include <Arduino.h>
+#include <App.hpp>
+#include <SimpleWiFi.h>
+#include <CameraSetup.h>
+#include <CameraServer.h>
+
+time_t checkTimestamp = 0l;
+
+void setup() {
+  pinMode( BOARD_LED, OUTPUT );
+  pinMode( FLASH_LED, OUTPUT );
+  digitalWrite( BOARD_LED, true );  
+  digitalWrite( FLASH_LED, false );  
+  Serial.begin(115200);
+  delay( 3000 ); // wait for serial monitor
+  Serial.setDebugOutput(true);
+  Serial.println( "\n\n\n" APP_NAME " - Version " APP_VERSION );
+  Serial.println( "Build date: " __DATE__ " " __TIME__ "\n");
+
+  Serial.println();
+  Serial.printf("CPU Frequency       : %dMHz\n", ESP.getCpuFreqMHz() );
+  Serial.println();
+
+  initializeCamera();
+
+  // digitalWrite( FLASH_LED, true );  
+  disableWiFi();
+  // digitalWrite( FLASH_LED, false );  
+  connectWiFi();
+
+  startCameraServer();
+
+  Serial.print("Camera Ready! Use 'http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("' to connect");
+}
+
+void loop()
+{
+  time_t currentTimestamp = millis();
+
+  if (( currentTimestamp - checkTimestamp ) >= 10000 )
+  {
+    if( WiFi.isConnected() == false )
+    {
+      connectWiFi();
+    }
+    checkTimestamp = currentTimestamp;
+  }
+  delay(25);
+}
---
+++ examples/ESP32-TTGO-T-Display-Test1/README.md
@@ -0,0 +1,2 @@
+# ESP32-TTGO-T-Display-Test1
+ 
